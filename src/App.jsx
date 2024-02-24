998835a2613e10f509d65663e2e4de948b8100e1
gopalk26 <gopalakrishnakimid@gmail.com>
2024-02-24 14:05:16 +0530

    first Commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+       
+            <h1 className="text-center text-rose-700  text-3xl"> hello world fvvdfbdnffbhrtfngrrhtjthrjftj</h1> 
+       
+      </div>
+      
+    </>
+  )
+}
+
+export default App
